7679d4fe32226e9cfd3bcdd55336364d45d81fe1
phoenixtype <samuelakuma130@gmail.com>
2022-10-22 22:54:58 -0400

    html and css

---
+++ script.js
@@ -0,0 +1,107 @@
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+const currTime = document.querySelector('#currTime');
+const durTime = document.querySelector('#durTime');
+
+//Song Title
+const songs = ['hey', 'summer', 'ukulele'];
+
+//Keep track of Song
+let songIndex = 2;
+
+//Initially load song details into DOM
+loadSong(songs[songIndex]);
+
+//Update song details
+function loadSong(song) {
+    title.innerText = song;
+    audio.src = `music/${song}.mp3`;
+    cover.src = `images/${song}.jpg`;
+}
+
+//Play song
+function playSong() {
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+    audio.play();
+}
+
+//Pause song
+function pauseSong() {
+   musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+    audio.pause();
+}
+
+//Previous song
+function prevSong() {
+    songIndex--;
+
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+//Next song
+function prevSong() {
+    songIndex++;
+
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+//Update progress bar
+function updateProgress(e) { 
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+}
+//Set progress bar
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
+}
+
+//Get duration & currentTime for time of song
+    //define minutes currentTime
+    //define seconds currentTime
+    //change currentTime DOM
+    //define minutes duration
+    //define seconds duration
+    //change duration DOM
+
+//Event listeners
+
+//Change song
+
+//Time/Song update
+
+//Click on progress bar
+
+//Song ends
+
+//Time of song
+
